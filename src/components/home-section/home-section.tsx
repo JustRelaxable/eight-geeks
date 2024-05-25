2f7a7e909960395a9ba0c5c7d9737cd2de27117c
Taha Sökmen <resimli1000@gmail.com>
2024-05-25 21:14:19 +0300

    create home section component

---
+++ src/components/home-section/home-section.tsx
@@ -0,0 +1,16 @@
+import styles from "./home-section.module.css";
+
+export default function HomeSection() {
+  return (
+    <section>
+      <header className={styles.headerContainer}>
+        <div>
+          <p>Games</p>
+        </div>
+        <div>
+          <p>View all games</p>
+        </div>
+      </header>
+    </section>
+  );
+}
--- src/routes/home/home.tsx
+++ src/routes/home/home.tsx
@@ -1,5 +1,13 @@
+import HomeSection from "../../components/home-section/home-section";
 import styles from "./home.module.css";
 
 export default function Home() {
-  return <p>Hello from home!</p>;
+  return (
+    <main>
+      <HomeSection />
+      <HomeSection />
+      <HomeSection />
+      <HomeSection />
+    </main>
+  );
 }
