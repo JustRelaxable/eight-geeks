4a8c317ef1ffecee7190a1932fa4d16b9a5fb826
Taha Sökmen <resimli1000@gmail.com>
2024-05-25 20:52:34 +0300

    create navigation bar component

---
+++ src/components/navigation-bar/navigation-bar.tsx
@@ -0,0 +1,5 @@
+import styles from "./navigation-bar.module.css";
+
+export default function NavigationBar() {
+  return <nav className={styles.container}></nav>;
+}
--- src/routes/root/root.tsx
+++ src/routes/root/root.tsx
@@ -1,8 +1,10 @@
+import NavigationBar from "../../components/navigation-bar/navigation-bar";
 import styles from "./root.module.css";
 import { Outlet } from "react-router-dom";
 export default function Root() {
   return (
     <div className={styles.container}>
+      <NavigationBar />
       <Outlet />
     </div>
   );
