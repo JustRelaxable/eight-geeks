1c60eb06e0a10aa85adfd242d1d39b249390c7ad
Taha Sökmen <resimli1000@gmail.com>
2024-05-25 19:24:00 +0300

    create generic error page

--- src/error-page.tsx
+++ src/error-page.tsx
@@ -1,15 +1,20 @@
-import { useRouteError } from "react-router-dom";
+import { useRouteError, isRouteErrorResponse } from "react-router-dom";
 
 export default function ErrorPage() {
   const error = useRouteError();
-  console.error(error);
+  console.error("error:", error);
+
+  let statusText = "";
+  if (isRouteErrorResponse(error)) {
+    statusText = error.statusText;
+  }
 
   return (
     <div id="error-page">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
-        <i>{error.statusText || error.message}</i>
+        <i>{statusText}</i>
       </p>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import ErrorPage from "./error-page";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -12,6 +13,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <div>Hello world!</div>,
+    errorElement: <ErrorPage />,
   },
 ]);
 root.render(
