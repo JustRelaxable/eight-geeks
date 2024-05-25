5742b035f7dfddf387d42c80c40eb89409f2eef1
Taha Sökmen <resimli1000@gmail.com>
2024-05-25 22:32:11 +0300

    home section improvements

--- src/components/home-section/home-section.tsx
+++ src/components/home-section/home-section.tsx
@@ -1,10 +1,23 @@
 import HomeSectionHeader from "./header/home-section-header";
 import styles from "./home-section.module.css";
+import HomeSectionItem from "./item/home-section-item";
+import placeholderThumbnail from "./item/placeholder-home-section-item-thumbnail.avif";
 
 export default function HomeSection() {
   return (
     <section>
       <HomeSectionHeader sectionName="Games" />
+      <ul className={styles.itemContainer}>
+        {Array.from(Array(10)).map((x) => (
+          <li>
+            <HomeSectionItem
+              thumbnailSource={placeholderThumbnail}
+              itemName="The Emily's Adventures"
+              itemAuthor="Taha Sökmen"
+            />
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
--- src/components/home-section/item/home-section-item.tsx
+++ src/components/home-section/item/home-section-item.tsx
@@ -1,17 +1,19 @@
 import styles from "./home-section-item.module.css";
 
 export default function HomeSectionItem({
+  thumbnailSource,
   itemName,
   itemAuthor,
 }: {
+  thumbnailSource: string;
   itemName: string;
   itemAuthor: string;
 }) {
   return (
     <article className={styles.container}>
-      <img className={styles.thumbnail} />
+      <img src={thumbnailSource} className={styles.thumbnail} />
       <p>{itemName}</p>
-      <p>{itemAuthor}</p>
+      <p className={styles.authorText}>{itemAuthor}</p>
     </article>
   );
 }
