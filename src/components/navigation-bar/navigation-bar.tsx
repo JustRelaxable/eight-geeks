6072a515145ad62696f683d0f41b4dba3ce2d128
Taha Sökmen <resimli1000@gmail.com>
2024-05-26 13:04:22 +0300

    add navbar logo

--- src/components/navigation-bar/navigation-bar.tsx
+++ src/components/navigation-bar/navigation-bar.tsx
@@ -1,5 +1,15 @@
+import EightGeeksLogo from "../svg/eight-geeks-logo";
 import styles from "./navigation-bar.module.css";
 
 export default function NavigationBar() {
-  return <nav className={styles.container}></nav>;
+  return (
+    <nav className={styles.container}>
+      <div className={styles.logoContainer}>
+        <EightGeeksLogo />
+        <p className={styles.logoText}>
+          Eight <br /> Geeks
+        </p>
+      </div>
+    </nav>
+  );
 }
